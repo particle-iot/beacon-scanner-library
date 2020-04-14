a4210f996359a2e19254cfd93a4f9a6304dc66ab
Mariano Goluboff <mgolu@users.noreply.github.com>
2020-04-14 16:39:26 -0400

    Fix temp on Kontakt

--- src/kontaktTag.h
+++ src/kontaktTag.h
@@ -20,7 +20,7 @@ public:
 
     BleAddress getAddress() { return address; };
     uint8_t getBattery() { return battery; };
-    uint8_t getTemperature() { return temperature; };
+    int8_t getTemperature() { return temperature; };
     uint8_t getAccelSensitivity() { return accel_sensitivity; };
     uint16_t getButtonTime() { return button_time; };
     uint16_t getAccelLastDoubleTap() { return accel_last_double_tap; };
@@ -31,9 +31,9 @@ public:
 
 private:
     BleAddress address;
-    uint8_t battery, temperature, accel_sensitivity;
+    uint8_t battery, accel_sensitivity;
     uint16_t button_time, accel_last_double_tap, accel_last_movement;
-    int8_t x_axis, y_axis, z_axis;
+    int8_t x_axis, y_axis, z_axis, temperature;
     bool accel_data;
 };
 
