a7d2b129ee33b15f0bb094e3304085b640a97c30
Mariano Goluboff <mgolu@users.noreply.github.com>
2021-06-20 08:39:09 -0400

    Update library and documentation

--- examples/lairdBt510Log/lairdBt510Log.ino
+++ examples/lairdBt510Log/lairdBt510Log.ino
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2020 Particle Industries, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include "Particle.h"
 #include "BeaconScanner.h"
 
@@ -7,6 +23,11 @@ SYSTEM_MODE(SEMI_AUTOMATIC);
 SerialLogHandler logHandler(LOG_LEVEL_TRACE);
 
 void eventCallback(LairdBt510& beacon, lairdbt510_event_type evt) {
+  /**
+   * This callback receives events from the Laird BT510 beacon.
+   * See the documentation for the beacon to see the difference between
+   * events and alarms.
+   */
   switch (evt)
   {
   case lairdbt510_event_type::TEMPERATURE:
@@ -32,6 +53,13 @@ void eventCallback(LairdBt510& beacon, lairdbt510_event_type evt) {
 }
 
 void alarmCallback(LairdBt510& beacon, lairdbt510_event_type alarm) {
+  /**
+   * This callback receives alarms from the Laird BT510 beacon. It's important
+   * to note that the library doesn't implement alarm filtering or hysteresis.
+   * If the beacon sends an alarm, it is passed to the callback. The appplication
+   * may implement some filtering (or set/clear hysteresis) to avoid repeat
+   * alerting, if that's desirable.
+   */
   switch (alarm)
   {
     case lairdbt510_event_type::MOVEMENT:
@@ -50,6 +78,12 @@ void alarmCallback(LairdBt510& beacon, lairdbt510_event_type alarm) {
 }
 
 int configDevice(String command) {
+  /**
+   * Function that receives a JSON object to configure the nearby beacons
+   * 
+   * The field names are the ones that can be set on the beacon. Refer
+   * to Laird's data sheet for more details on what they each do.
+   */
   JSONValue obj = JSONValue::parseCopy(command);
   JSONObjectIterator iter(obj);
   LairdBt510Config config;
@@ -79,12 +113,26 @@ int configDevice(String command) {
     else if (iter.name() == "deltaTemperatureAlarmThreshold") {
       config.deltaTempAlarm(iter.value().toInt());
     }
+    /**
+     * The passkey field is the current passkey to be used for the connection.
+     * It doesn't change the configuration on the device.
+     */
     else if (iter.name() == "passkey") {
       config.currentPasskey(iter.value().toString().data());
     }
+    /**
+     * The newPasskey field is the new passkey to be configured onto the device.
+     * This changes the configuration.
+     */
     else if (iter.name() == "newPasskey") {
       config.newPasskey(iter.value().toString().data());
     }
+    /**
+     * The sensorAddress field doesn't change anything on the beacon itself.
+     * Rather, this field is set in order to specify the address of the beacon
+     * that should be configured. If omitted, then all the detected beacons
+     * nearby will be reconfigured.
+     */
     else if (iter.name() == "sensorAddress") {
       auto addressString = String(iter.value().toString().data());
       address.set(addressString, BleAddressType::RANDOM_STATIC);
