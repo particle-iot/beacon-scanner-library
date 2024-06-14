f9c93cd22f7f787173919aca9818d03d81cff34f
gusgonnet <gusgonnet@gmail.com>
2024-06-13 22:09:16 -0400

    shelly ble devices showing up in scan

--- src/shelly.cpp
+++ src/shelly.cpp
@@ -25,8 +25,8 @@ void Shelly::populateData(const BleScanResult *scanResult)
     uint8_t custom_data[BLE_MAX_ADV_DATA_LEN];
     ADVERTISING_DATA(scanResult).customData(custom_data, sizeof(custom_data));
     snprintf(uuid, sizeof(uuid), "%02X%02X%02X%02X-%02X%02X-%02X%02X-%02X%02X-%02X%02X%02X%02X%02X%02X",
-                custom_data[4], custom_data[5], custom_data[6], custom_data[7], custom_data[8], custom_data[9], custom_data[10], custom_data[11], custom_data[12],
-                custom_data[13], custom_data[14], custom_data[15], custom_data[16], custom_data[17], custom_data[18], custom_data[19]);
+             custom_data[4], custom_data[5], custom_data[6], custom_data[7], custom_data[8], custom_data[9], custom_data[10], custom_data[11], custom_data[12],
+             custom_data[13], custom_data[14], custom_data[15], custom_data[16], custom_data[17], custom_data[18], custom_data[19]);
     power = (int8_t)custom_data[24];
 }
 
@@ -34,40 +34,42 @@ bool Shelly::isBeacon(const BleScanResult *scanResult)
 {
     uint8_t custom_data[BLE_MAX_ADV_DATA_LEN];
 
-    if (ADVERTISING_DATA(scanResult).customData(custom_data, sizeof(custom_data)) == 25)
-    {
-        if (custom_data[0] == 0x4c && custom_data[1] == 0x00 && custom_data[2] == 0x02 && custom_data[3] == 0x15)
-        {
-            return true;
-        }
-    }
+    uint8_t buf[BLE_MAX_ADV_DATA_LEN];
+    uint8_t count = ADVERTISING_DATA(scanResult).get(BleAdvertisingDataType::SERVICE_DATA, buf, BLE_MAX_ADV_DATA_LEN);
+    if (count > 3 && buf[0] == 0xD2 && buf[1] == 0xFC) // Shelly UUID is 0xFCD2
+        return true;
     return false;
 }
 
 void Shelly::toJson(JSONWriter *writer) const
 {
-        writer->name(address.toString()).beginObject();
-        writer->name("uuid").value(getUuid());
-        writer->name("power").value(getPower());
-        writer->name("rssi").value(getRssi());
-        writer->endObject();
+    writer->name(address.toString()).beginObject();
+    writer->name("uuid").value(getUuid());
+    writer->name("power").value(getPower());
+    writer->name("rssi").value(getRssi());
+    writer->endObject();
 }
 
 void Shelly::addOrUpdate(const BleScanResult *scanResult)
 {
     uint8_t i;
-    for (i = 0; i < beacons.size(); ++i) {
-        if (beacons.at(i).getAddress() == ADDRESS(scanResult)) {
+    for (i = 0; i < beacons.size(); ++i)
+    {
+        if (beacons.at(i).getAddress() == ADDRESS(scanResult))
+        {
             break;
         }
     }
-    if (i == beacons.size()) {
+    if (i == beacons.size())
+    {
         Shelly new_beacon;
         new_beacon.populateData(scanResult);
         new_beacon.missed_scan = 0;
         beacons.append(new_beacon);
-    } else {
-        Shelly& beacon = beacons.at(i);
+    }
+    else
+    {
+        Shelly &beacon = beacons.at(i);
         beacon.newly_scanned = false;
         beacon.populateData(scanResult);
         beacon.missed_scan = 0;
